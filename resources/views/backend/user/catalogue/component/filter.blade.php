30a98767b5cef3c6f5a713003b7dd5aa337c26c4
kevinthang2k1 <kevinthang2k1@gmail.com>
2023-12-19 09:21:15 +0700

    update

--- resources/views/backend/user/catalogue/component/filter.blade.php
+++ resources/views/backend/user/catalogue/component/filter.blade.php
@@ -19,7 +19,6 @@
                         $publish = request('publish') ?: old('publish');
                     @endphp
                     <select name="publish" class="form-control mr10 setupSelect2">
-                        <option value="-1" selected="selected">Chọn tình trạng</option>
                         @foreach(config('apps.general.publish') as $key => $val)
                             <option {{ ($publish == $key) ? 'selected' : '' }} value="{{ $key }}">{{ $val }}</option>
                         @endforeach
